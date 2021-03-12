250232b362f89145824bacdccee2c9f265d17186
Kyle Walsh <kew96@cornell.edu>
2021-03-12 13:06:32 -0500

    Script to retrieve SP500 Consumer Staples tickers

---
+++ src/data/current_consumer_staples.py
@@ -0,0 +1,12 @@
+from pathlib import Path
+
+import pandas as pd
+
+
+DATA_PATH = Path().cwd().parent.parent.joinpath('data')
+
+sp500 = pd.read_html('https://en.wikipedia.org/wiki/List_of_S%26P_500_companies')[0]
+consumer_staples = sp500[sp500['GICS Sector'] == 'Consumer Staples']
+
+with open(DATA_PATH.joinpath('raw/consumer_staples.txt'), 'w') as file:
+    file.writelines(ticker+'\n' for ticker in consumer_staples.Symbol)
