77a41a41d19f5faca238faa6bff47b40ed7b7855
Kyle Walsh <kew96@cornell.edu>
2021-05-14 10:58:38 -0400

    Updated set operations, parameter passing, and commenting

--- src/data/get_all_tickers.py
+++ src/data/get_all_tickers.py
@@ -1,19 +1,23 @@
+"""
+Combines all tickers from the consumer staples portfolios and tickers that are currently in the S&P500
+"""
 from pathlib import Path
 
 import pandas as pd
 
 DATA_PATH = Path(__file__).parent.parent.parent.joinpath('data')
 
+# Use set so that there are no duplicates
 ticker_set = set()
 
-for csv in DATA_PATH.joinpath('interim').glob('*_holdings_values.txt'):
-    df = pd.read_table(csv)
-    for v in list(df.columns):
-        ticker_set.add(v)
+# Add all tickers from portfolios
+for txt_file in DATA_PATH.joinpath('interim').glob('*_holdings_values.txt'):
+    df = pd.read_table(txt_file)
+    ticker_set.update(df.columns)
 
+# Add all tickers currently in consumer staples
 sp_consumer_staples = pd.read_table(DATA_PATH.joinpath('raw', 'consumer_staples_cusips.txt'))
-for ticker in sp_consumer_staples.TICKER:
-    ticker_set.add(ticker)
+ticker_set.update(sp_consumer_staples.TICKER)
 
-with open(DATA_PATH.joinpath('interim/all_tickers.txt'), 'w') as file:
+with open(DATA_PATH.joinpath('interim', 'all_tickers.txt'), 'w') as file:
     file.write('\n'.join(ticker_set))
