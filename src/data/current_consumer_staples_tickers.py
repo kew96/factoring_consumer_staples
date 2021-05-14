cfbae2d3e24094ed5cb6aca81bbcca9a2f306b3e
Kyle Walsh <kew96@cornell.edu>
2021-05-14 10:42:20 -0400

    Updated parameter passing

--- src/data/current_consumer_staples_tickers.py
+++ src/data/current_consumer_staples_tickers.py
@@ -8,5 +8,5 @@ DATA_PATH = Path(__file__).parent.parent.parent.joinpath('data')
 sp500 = pd.read_html('https://en.wikipedia.org/wiki/List_of_S%26P_500_companies')[0]
 consumer_staples = sp500[sp500['GICS Sector'] == 'Consumer Staples']
 
-with open(DATA_PATH.joinpath('raw/consumer_staples.txt'), 'w') as file:
+with open(DATA_PATH.joinpath('raw', 'consumer_staples.txt'), 'w') as file:
     file.writelines(ticker.replace('.', '')+'\n' for ticker in consumer_staples.Symbol)
