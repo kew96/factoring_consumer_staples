1e6037d618e90590b3095283735abb1599f58912
Kyle Walsh <kew96@cornell.edu>
2021-05-14 10:40:41 -0400

    Updated inline comments and parameter passing

--- src/data/clean_holdings.py
+++ src/data/clean_holdings.py
@@ -7,7 +7,8 @@ DATA_PATH = Path(__file__).parent.parent.parent.joinpath('data')
 
 all_holdings = pd.read_table(DATA_PATH.joinpath('raw/holdings.txt'))
 
-for portfolio in all_holdings.crsp_portno.unique():
-    holdings = all_holdings[all_holdings.crsp_portno == portfolio]
+for portfolio in all_holdings.crsp_portno.unique(): # Iterate through each unique portfolio
+    holdings = all_holdings[all_holdings.crsp_portno == portfolio] # Subset the data corresponding to the portfolio
+    # Use the date as the index and each asset as the column
     holdings_pivot = holdings.pivot_table(index='eff_dt', columns='ticker', values='market_val')
-    holdings_pivot.to_csv(DATA_PATH.joinpath(f'interim/{portfolio}_holdings_values.txt'), index_label=False, sep='\t')
+    holdings_pivot.to_csv(DATA_PATH.joinpath('interim', f'{portfolio}_holdings_values.txt'), index_label=False, sep='\t')
