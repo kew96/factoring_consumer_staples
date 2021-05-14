7d88cd081b1139b476ef018db70b791fc61538af
Kyle Walsh <kew96@cornell.edu>
2021-05-14 13:57:05 -0400

    Refactoring, commenting, recreating data

--- src/data/clean_market_values.py
+++ src/data/clean_market_values.py
@@ -22,12 +22,13 @@ market_values = market_values.dropna(subset=['tic'])
 
 market_values.cshoc = market_values.cshoc * 1_000
 
-market_values.prccm = market_values.prccm.interpolate(method='slinear', limit=2, limit_area='outside')
-market_values.cshoc = market_values.cshoc.interpolate(method='pad', limit=2, limit_area='outside')
+market_values = market_values.sort_values(['tic', 'datadate']).reset_index(drop=True)
 
 for ticker in market_values.tic.unique():
     subset = market_values[market_values.tic == ticker]
-    # Linearly interpolate prices
+    subset.prccm = subset.prccm.interpolate(method='slinear', limit=2, limit_area='outside')
+    subset.cshoc = subset.cshoc.interpolate(method='pad', limit=2, limit_area='outside')
+    # Linearly interpolate period end prices
     end_prices = subset.prccm[-4:]
     if end_prices.isna().sum() == 2:
         # If there are two missing values and they are the last two values
@@ -45,82 +46,20 @@ for ticker in market_values.tic.unique():
 # Calculating monthly market value
 market_values['mkvaltq'] = market_values.prccm * market_values.cshoc
 
-# Finding all tickers with unexpected missing values
-def get_bad_data(values):
-    tickers = list()
-    for ticker in values.tic.unique():
-        subset = values[values.tic == ticker]
-        current = 'na' if np.isnan(subset.mkvaltq.iloc[0]) else 'val'
-        for v in subset.mkvaltq[1:]:
-            if not np.isnan(v):
-                current = 'val'
-            elif current == 'val' and np.isnan(v):
-                tickers.append(ticker)
-                continue
-    return Counter(tickers)
 
-bad_data = get_bad_data(market_values)
+# Reduces data to only months that are quarter end and that have a date after the 25th
 
-print(bad_data)
-#
-# # One period missing market values imputation, assumes linearity
-# def impute_one_period(data):
-#     if np.isnan(data.iloc[0]): # If the first period is missing
-#         diff = data.iloc[2] - data.iloc[1] # assume constant growth
-#         return data.iloc[1] - diff
-#     elif np.isnan(data.iloc[1]): # If the middle value is missing
-#         return (data.iloc[0] + data.iloc[2]) / 2 # take average of previous and next period
-#     else: # If the last period is missing
-#         diff = data.iloc[1] - data.iloc[0] # assume constant growth
-#         return data.iloc[1] + diff
-#
-# market_values = market_values.sort_values(['tic', 'datadate']).reset_index(drop=True)
-#
-# for elem in bad_data.items():
-#     subset = market_values[market_values.tic == elem[0]]
-#     for ind, value in zip(subset.index, subset.mkvaltq):
-#         if np.isnan(value):
-#             if ind == subset.index[0]:
-#                 three_set = subset.loc[ind:ind+3, 'mkvaltq']
-#             elif ind == subset.index[-1]:
-#                 three_set = subset.loc[ind-2:ind, 'mkvaltq']
-#             else:
-#                 three_set = subset.loc[ind-2:ind+1, 'mkvaltq']
-#             if np.sum(np.isnan(three_set)) == 1:
-#                 try:
-#                     new_value = impute_one_period(three_set)
-#                     subset.loc[ind, 'mkvaltq'] = new_value
-#                 except:
-#                     print(subset.info())
-#                     raise Exception
-#     market_values[market_values.tic == elem[0]] = subset
-#
-# # Reduces data to only months that are quarter end and that have a date after the 25th
-#
-# quarter_market_vals = market_values[market_values.datadate.dt.month.isin([3, 6, 9, 12])]
-# quarterly_vals = quarter_market_vals[quarter_market_vals.datadate.dt.day > 25]
-#
-# # Iterates through all dates to find the latest date available since quarters may not end on an available day
-#
-# good_dates = list()
-# for year in range(2000, 2021):
-#     for month in [3, 6, 9, 12]:
-#         last_day = pd.Timestamp(year, month, 1) + relativedelta(months=1, days=-1)
-#         while not last_day in quarterly_vals.datadate.values:
-#             last_day = last_day - relativedelta(days=1)
-#         good_dates.append(last_day)
-#
-# quarterly_vals = quarterly_vals[quarterly_vals.datadate.isin(good_dates)]
-#
-# # Ensures each asset has at least three data points
-#
-# little_data = list()
-# for ticker in quarterly_vals.tic.unique():
-#     if len(quarterly_vals[quarterly_vals.tic==ticker]) < 2:
-#         little_data.append(ticker)
-#
-# quarterly_vals = quarterly_vals[~quarterly_vals.tic.isin(little_data)]
-#
-# quarterly_vals.to_csv(DATA_PATH.joinpath('interim', 'market_values.txt'), index=False, sep='\t')
-#
-# print('Cleaned market values!')
+quarterly_vals = market_values[market_values.datadate.str.contains('|'.join(['-03', '-06', '-09', '-12']))]
+
+# Ensures each asset has at least three data points
+
+little_data = list()
+for ticker in quarterly_vals.tic.unique():
+    if len(quarterly_vals[quarterly_vals.tic==ticker]) < 2:
+        little_data.append(ticker)
+
+quarterly_vals = quarterly_vals[~quarterly_vals.tic.isin(little_data)]
+
+quarterly_vals.to_csv(DATA_PATH.joinpath('interim', 'market_values.txt'), index=False, sep='\t')
+
+print('Cleaned market values!')
