5511f30487b6ffbc3892937bc9b1bb4b12a197a6
Kyle Walsh <kew96@cornell.edu>
2021-05-15 12:25:53 -0400

    Refactoring, recleaning data, changing to book to market ratio

--- src/data/clean_book_ratios.py
+++ src/data/clean_book_ratios.py
@@ -15,149 +15,46 @@ DATA_PATH = Path(__file__).parent.parent.parent.joinpath('data')
 # Loading all data
 
 book_ratios = pd.read_table(DATA_PATH.joinpath('raw', 'prices_assets_liabilities_quarterly.txt'),
-                            parse_dates=['datadate'])
+                            parse_dates=['datadate'], usecols=['datadate', 'tic', 'conm', 'atq', 'ltq'])
 book_ratios['book_value'] = (book_ratios.atq - book_ratios.ltq) * 1_000_000
+book_ratios.datadate = book_ratios.datadate.dt.strftime('%Y-%m')
 
 shares = pd.read_table(DATA_PATH.joinpath('raw', 'prices_shares_outstanding_monthly.txt'),
-                       usecols=['date', 'TICKER', 'SHROUT'], parse_dates=['date'])
-shares.columns = ['datadate', 'tic', 'cshoc']
-shares = shares.dropna(subset=['tic'])
+                       usecols=['date', 'TICKER', 'SHROUT', 'PRC'], parse_dates=['date'])
+shares.columns = ['datadate', 'tic', 'prccq', 'cshoc']
+shares.datadate = shares.datadate.dt.strftime('%Y-%m')
 
 book_ratios = book_ratios.merge(shares, on=['datadate', 'tic'], how='left')
+book_ratios = book_ratios.dropna(subset=['tic'])
 
-nan_shares = list()
-for entry in book_ratios.cshoc:
-    try:
-        nan_shares.append(float(entry) * 1_000)
-    except ValueError:
-        nan_shares.append(np.nan)
+book_ratios.cshoc = book_ratios.cshoc * 1_000
 
-book_ratios.cshoc = nan_shares
+book_ratios = book_ratios.sort_values(['tic', 'datadate']).reset_index(drop=True)
 
-# Imputes missing shares at the end of a period by taking the most recent value
+for ticker in book_ratios.tic.unique():
+    subset = book_ratios[book_ratios.tic == ticker]
+    subset.prccm = subset.prccq.interpolate(method='slinear', limit=2, limit_area='outside')
+    subset.cshoc = subset.cshoc.interpolate(method='pad', limit=2, limit_area='outside')
+    # Linearly interpolate period end prices
+    end_btm = subset.prccq[-4:]
+    if end_btm.isna().sum() == 2:
+        # If there are two missing values and they are the last two values
+        if np.isnan(end_btm.iloc[2]) and np.isnan(end_btm.iloc[3]):
+            step = end_btm.iloc[1] - end_btm.iloc[0]
+            end_btm.iloc[2] = end_btm.iloc[1] + step
+            end_btm.iloc[3] = end_btm.iloc[1] + 2 * step
+    elif end_btm.isna().sum() == 1:
+        # If there is one missing value and it is the last value
+        if np.isnan(end_btm.iloc[-1]):
+            step = end_btm.iloc[-2] - end_btm.iloc[-3]
+            end_btm.iloc[-1] = end_btm.iloc[-2] + step
+    book_ratios[book_ratios.tic == ticker] = subset
 
-def fix_missing_shares(df):
-    bad_tickers = list()
-    for ticker in tqdm(df.tic.unique(), desc='tickers'):
-        subset = shares[shares.tic==ticker].dropna(subset=['cshoc'])
-        if subset.empty:
-            bad_tickers.append(ticker)
-            continue
-        for row in tqdm(df[df.tic==ticker].itertuples(), desc=ticker, leave=False):
-            if np.isnan(row.cshoc):
-                val = np.nan
-                dt = row.datadate
-                if row.datadate.year == 2000 and row.datadate.month == 3:
-                    while np.isnan(val):
-                        dt = dt + relativedelta(days=1)
-                        if dt > date(2021, 4, 1):
-                            val = subset[subset.datadate==subset.datadate.max()].cshoc.values[0]
-                        else:
-                            vals_array = subset[subset.datadate==dt].cshoc.values
-                            val = vals_array[0] if len(vals_array) else np.nan
-                else:
-                    while np.isnan(val):
-                        dt = dt - relativedelta(days=1)
-                        if dt < date(2000, 1, 1):
-                            val = subset[subset.datadate==subset.datadate.min()].cshoc.values[0]
-                        else:
-                            vals_array = subset[subset.datadate==dt].cshoc.values
-                            val = vals_array[0] if len(vals_array) else np.nan
-                df.loc[row.Index, 'cshoc'] = val
-    return df[~df.tic.isin(bad_tickers)]
+# Calculating monthly market value
+book_ratios['mkvaltq'] = book_ratios.prccq * book_ratios.cshoc
 
-book_ratios = fix_missing_shares(book_ratios)
-
-# Book value per share = book value / shares outstanding
-
-book_ratios['bv_per_share'] = book_ratios.book_value / book_ratios.cshoc
-
-# Price to book ratio = price / book value per share
-
-book_ratios['ptb'] = book_ratios.prccq / book_ratios.bv_per_share
-
-# Retrieves all data values with missing values and prints the ticker, name, and number of missing points
-# skips all missing values at the beginning and end
-
-def get_bad_data(book_ratios):
-    bad_data = list()
-    for ticker in book_ratios.tic.unique():
-        subset = book_ratios[book_ratios.tic==ticker]
-        if np.isnan(subset.ptb.iloc[-1]):
-            subset = subset.iloc[:-1]
-        current = 'na' if np.isnan(subset.ptb.iloc[0]) else 'val'
-        for v in subset.ptb[1:]:
-            if not np.isnan(v):
-                current = 'val'
-            elif current == 'val' and np.isnan(v):
-                name = subset.conm.values[0]
-                bad_data.append((ticker, name))
-                continue
-    return Counter(bad_data)
-
-bad_data_1 = get_bad_data(book_ratios)
-
-# Imputes the missing data for two periods, assumes linearity
-
-def impute_two_periods(data):
-    # print(data)
-    if np.sum(np.isnan(data.iloc[:2])) == 2: # If first two periods missing
-        diff = data.iloc[3] - data.iloc[2] # assume trend holds
-        return data.iloc[2] - diff, data.iloc[2] - diff * 2
-    elif np.sum(np.isnan(data.iloc[2:])) == 2: # If last two periods missing
-        diff = data.iloc[1] - data.iloc[0] # assume trend holds
-        return data.iloc[1] + diff, data.iloc[1] + diff * 2
-    elif np.sum(np.isnan(data.iloc[1:3])): # If middle two periods missing
-        diff = data.iloc[3] - data.iloc[0] # assume constant growth/decay
-        step = diff / 3
-        return data.iloc[0] + step, data.iloc[0]  + step * 2
-    # Individual case handled later
-
-
-for elem in bad_data_1.items():
-    subset = book_ratios[book_ratios.tic == elem[0]]
-    for ind, value1, value2 in zip(subset.index, subset.ptb[:-1], subset.ptb[1:]):
-        if np.sum(np.isnan([value1, value2])) == 2:
-            if ind == subset.index[0]:
-                data = subset.loc[ind:ind+4, 'ptb']
-            elif ind == subset.index[-2]:
-                data = subset.loc[ind-2:, 'ptb']
-            else:
-                data = subset.loc[ind-1:ind+3, 'ptb']
-            if np.sum(np.isnan(data)) > 2:
-                continue
-            else:
-                new_vals = impute_two_periods(data)
-            subset.loc[ind:ind+2, 'ptb'] = new_vals
-    book_ratios[book_ratios.tic == elem[0]] = subset
-
-# Imputes the missing data for one period, assumes linearity
-
-def impute_one_period(data):
-    if np.isnan(data.iloc[0]): # If the first period is missing
-        diff = data.iloc[2] - data.iloc[1] # assume constant growth
-        return data.iloc[1] - diff
-    elif np.isnan(data.iloc[1]): # If the middle value is missing
-        return (data.iloc[0] + data.iloc[2]) / 2 # take average of previous and next period
-    else: # If the last period is missing
-        diff = data.iloc[1] - data.iloc[0] # assume constant growth
-        return data.iloc[1] + diff
-
-
-for elem in bad_data_1.items():
-    subset = book_ratios[book_ratios.tic == elem[0]]
-    for ind, value in zip(subset.index, subset.ptb):
-        if np.isnan(value):
-            if ind == subset.index[0]:
-                three_set = subset.loc[ind:ind+3, 'ptb']
-            elif ind == subset.index[-1]:
-                three_set = subset.loc[ind-2:ind, 'ptb']
-            else:
-                three_set = subset.loc[ind-1:ind+1, 'ptb']
-            if np.sum(np.isnan(three_set)) == 1:
-                new_value = impute_one_period(three_set)
-                subset.loc[ind, 'ptb'] = new_value
-    book_ratios[book_ratios.tic == elem[0]] = subset
+# Book to market ratio = book value / market value
+book_ratios['btm'] = book_ratios.book_value / book_ratios.mkvaltq
 
 # Ensures that each asset has at least three data points
 
@@ -167,7 +64,7 @@ for ticker in book_ratios.tic.unique():
         little_data.append(ticker)
 
 book_ratios = book_ratios[~book_ratios.tic.isin(little_data)]
-book_ratios = book_ratios.dropna(subset=['ptb'])
+book_ratios = book_ratios.dropna(subset=['btm'])
 
 book_ratios.to_csv(DATA_PATH.joinpath('interim', 'book_ratios.txt'), index=False, sep='\t')
 
