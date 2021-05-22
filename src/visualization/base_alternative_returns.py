8a5d729b730ddd6e1f5082f3703d4f310f2b1890
Kyle Walsh <kew96@cornell.edu>
2021-05-22 09:37:18 -0400

    Updating visualizations

--- src/visualization/base_alternative_returns.py
+++ src/visualization/base_alternative_returns.py
@@ -9,17 +9,19 @@ plt.style.use('fivethirtyeight')
 DATA_PATH = Path(__file__).parent.parent.parent.joinpath('data', 'processed', 'visualization')
 
 returns_data = pd.read_table(DATA_PATH.joinpath('performance_data.txt'), parse_dates={'date': ['year', 'month']})
+mod_returns_data = pd.read_table(DATA_PATH.joinpath('modified_alpha_performance_data.txt'),
+                                 parse_dates={'date': ['year', 'month']})
 
 fig = plt.figure(figsize=(15,10))
-plt.plot(returns_data.date, returns_data.actual_ret, label='Realized Return')
-plt.plot(returns_data.date, returns_data.expected_ret, label='Expected Return')
-plt.title('Actual vs. Expected Excess Return of Fama French 3-Factor Model')
+plt.plot(returns_data.date, returns_data.actual_ret, label='Base')
+plt.plot(mod_returns_data.date, mod_returns_data.expected_ret, label='Alternative')
+plt.title('Base vs. Alternative Model Excess Returns')
 plt.legend()
 plt.ylabel('Return')
 plt.xlabel('Date')
 plt.xticks(returns_data.date[1::2], labels=returns_data.date[1::2].dt.strftime('%y-%b'), rotation=-80)
 
-plt.savefig(DATA_PATH.joinpath('actual_vs_expected_returns.png'),
+plt.savefig(DATA_PATH.joinpath('base_vs_alternative_returns.png'),
             format='png',
             transparent=False,
             bbox_inches='tight',
