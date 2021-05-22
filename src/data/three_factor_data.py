7a89005fc271e77b3d221e8d484de3aa4171f442
Kyle Walsh <kew96@cornell.edu>
2021-05-21 21:42:58 -0400

    Smaller bands and number of points

--- src/data/three_factor_data.py
+++ src/data/three_factor_data.py
@@ -8,12 +8,12 @@ tfm = fama_french.ThreeFactorModel()
 
 return_data, weights_data = tfm.max_sharpe_portfolios(start_year=2005,
                                                       end_year=2020,
-                                                      num_points=1000,
+                                                      num_points=300,
                                                       min_variance=0,
-                                                      max_variance=5,
+                                                      max_variance=3,
                                                       universe_size=20,
                                                       return_sharpe=True,
                                                       return_weights=True)
 
-return_data.to_csv(DATA_PATH.joinpath('performance_data.txt'), sep='\t')
-weights_data.to_csv(DATA_PATH.joinpath('weights_data.txt'), sep='\t')
+return_data.to_csv(DATA_PATH.joinpath('modified_alpha_performance_data.txt'), sep='\t')
+weights_data.to_csv(DATA_PATH.joinpath('modified_alpha_weights_data.txt'), sep='\t')
